d06d9d18c973c81aed1e6f9f1a2fc539c5601d76
vind <imalukataki@gmail.com>
2023-12-14 18:54:15 +0300

    bem structure was added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import {fetchWeatherData, fetchImageByCityName} from './api.js'
 
 const searchBox = document.querySelector('.search-box')
-const input = document.querySelector('.search-input')
+const input = document.querySelector('.search-box__search-input')
 const infoBox = document.querySelector('.info-box')
 const mainBox = document.querySelector('.main-box')
-const infoText = document.querySelector('.info-text')
+const infoText = document.querySelector('.info-box__info-text')
 const daysBox = document.querySelector('.days-box')
-const dayCards = document.querySelectorAll('.day-card')
+const dayCards = document.querySelectorAll('.days-box__day-card')
 
 const iconsURL = 'https://openweathermap.org/img/wn/'
 const months = [
@@ -47,17 +47,17 @@ function insertWeatherData(data, indexInDataList = 0){
 
     mainBox.style.backgroundImage = `url(${data.cityImgLink})`
     mainBox.querySelector('.main-box__location').innerHTML = data.city.name
-    mainBox.querySelector('.main-box__bottom__weather-description').innerHTML = data.list[indexInDataList].weather[0].description
-    mainBox.querySelector('.main-box__bottom__temp').innerHTML = `${data.list[indexInDataList].main.temp.toFixed(0)}<span>°C</span>`
-    mainBox.querySelector('.main-box__top__date').innerHTML = 
+    mainBox.querySelector('.main-box__weather-description').innerHTML = data.list[indexInDataList].weather[0].description
+    mainBox.querySelector('.main-box__temp').innerHTML = `${data.list[indexInDataList].main.temp.toFixed(0)}<span>°C</span>`
+    mainBox.querySelector('.main-box__date').innerHTML = 
     `${date.getUTCDate()} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()}, ${date.toUTCString().match(/\s\d{2}:\d{2}/)}`
-    mainBox.querySelector('.main-box__bottom__weather-icon').src = `${iconsURL}${data.list[indexInDataList].weather[0].icon}@2x.png`
+    mainBox.querySelector('.main-box__weather-icon').src = `${iconsURL}${data.list[indexInDataList].weather[0].icon}@2x.png`
 
     dayCards.forEach((currentDayCard) => {
         let currentDayCardData = data.list[currentDayCard.id]
         currentDayCard.querySelector('img').src = `${iconsURL}${currentDayCardData.weather[0].icon}.png`
-        currentDayCard.querySelector('.week-day').innerHTML = weekDays[(new Date(currentDayCardData.dt * 1000)).getUTCDay()]
-        currentDayCard.querySelector('.day-card__temp').innerHTML = `${currentDayCardData.main.temp.toFixed(0)}<span>°C</span>`
+        currentDayCard.querySelector('.days-box__week-day').innerHTML = weekDays[(new Date(currentDayCardData.dt * 1000)).getUTCDay()]
+        currentDayCard.querySelector('.days-box__temp').innerHTML = `${currentDayCardData.main.temp.toFixed(0)}<span>°C</span>`
     })
 
 }
@@ -84,9 +84,9 @@ async function searchHandler(e){
         return ((timeStampDate.getUTCHours() == 12 && timeStampDate.getUTCDate() != (new Date(data.list[0].dt ** 1000)).getUTCDate()) || timeStamp == data.list[0])
     })
 
-    infoBox.classList.add('info-box-active')
-    infoBox.querySelector('.current-day-card').classList.remove('current-day-card')
-    dayCards[0].classList.add('current-day-card')
+    infoBox.classList.add('info-box_active')
+    infoBox.querySelector('.days-box__day-card_current')?.classList.remove('days-box__day-card_current')
+    dayCards[0].classList.add('days-box__day-card_current')
 
 
     insertWeatherData(data)
@@ -95,28 +95,22 @@ async function searchHandler(e){
 
 searchBox.addEventListener('submit', searchHandler)
 
-// input.addEventListener('keydown', (e)=>{
-//     if(e.key == 'Enter'){
-//         searchHandler()
-//     }
-// })
-
 
 infoBox.addEventListener('transitionend', () => {
-    infoText.classList.add('info-text-active')
-    daysBox.classList.add('days-box-active')
+    infoText.classList.add('info-box__info-text_active')
+    daysBox.classList.add('days-box_active')
     dayCards.forEach((dayCard)=>{
-        dayCard.classList.add('day-card-active')
+        dayCard.classList.add('days-box__day-card_active')
     })
-    mainBox.classList.add('main-box-active')
+    mainBox.classList.add('main-box_active')
 })
 
 
 dayCards.forEach((dayCards) => {
     dayCards.addEventListener('click', (e) => {
-        infoBox.querySelector('.current-day-card').classList.remove('current-day-card')
+        infoBox.querySelector('.days-box__day-card_current').classList.remove('days-box__day-card_current')
         console.log(e.currentTarget)
-        e.currentTarget.classList.add('current-day-card')
+        e.currentTarget.classList.add('days-box__day-card_current')
         insertWeatherData(data, e.currentTarget.id)
     })
 })
